3a575fce71f3b52033b4d1dda2bb59bf83060303
TheJeterLP <joey.peter1998@gmail.com>
2014-08-01 01:15:38 +0200

    Remove my BukkitTools Library, because the maven shade plugin is buggy and we don't need the library directly for this.

--- src/main/java/com/redthirddivision/bukkitgamelib/GamePlugin.java
+++ src/main/java/com/redthirddivision/bukkitgamelib/GamePlugin.java
@@ -20,19 +20,17 @@ import com.redthirddivision.bukkitgamelib.listeners.BukkitInventoryEvent;
 import com.redthirddivision.bukkitgamelib.listeners.BukkitMoveEvent;
 import com.redthirddivision.bukkitgamelib.listeners.BukkitQuitEvent;
 import com.redthirddivision.bukkitgamelib.listeners.BukkitSignEvent;
-import de.thejeterlp.bukkit.bukkittools.command.CommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * <strong>Project:</strong> R3DBukkitGameLib <br>
  * <strong>File:</strong> GamePlugin.java
- * 
+ *
  * @author <a href="http://jeter.vc-network.com">TheJeterLP</a>
  */
 public abstract class GamePlugin extends JavaPlugin {
 
     private GameManager manager;
-    private CommandManager cmdmanager;
 
     public abstract void onPluginStart();
 
@@ -41,7 +39,6 @@ public abstract class GamePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         manager = new GameManager(this);
-        cmdmanager = new CommandManager(this);
         getServer().getPluginManager().registerEvents(new BukkitInventoryEvent(), this);
         getServer().getPluginManager().registerEvents(new BukkitMoveEvent(), this);
         getServer().getPluginManager().registerEvents(new BukkitQuitEvent(), this);
@@ -56,10 +53,6 @@ public abstract class GamePlugin extends JavaPlugin {
 
     public GameManager getGameManager() {
         return manager;
-    }
-
-    public CommandManager getCommandManager() {
-        return cmdmanager;
     }
 
 }
